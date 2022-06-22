35f5a1740c01954a2ab63d30022df27d0ad3cef3
Matt De Vries <matthew.devries.13@gmail.com>
2022-06-22 18:31:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name="cellshape",
-    version="0.0.2",
+    version="0.0.3-rc0",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
