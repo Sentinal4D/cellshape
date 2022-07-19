591d2d7dbe45ade1412b259d87634931b0757d85
DeVriesMatt <matthew.devries.13@gmail.com>
2022-07-19 12:57:58 +0100

    Bump version: 0.0.21 → 0.0.22-rc0

--- cellshape/__init__.py
+++ cellshape/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.21"
+__version__ = "0.0.22-rc0"
 
 import cellshape_cloud as cloud
 import cellshape_cluster as cluster
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [
 
 setup(
     name="cellshape",
-    version="0.0.21",
+    version="0.0.22-rc0",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
